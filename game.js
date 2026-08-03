d95b5e5f1e2c997be85718a85c0f73f606a63e0b
archivebtw <archivebtw@gmail.com>
2026-08-04 00:31:45 +0300

    Delete game.js

--- game.js
+++
@@ -1,6 +0,0 @@
-let score=0;
-const scoreEl=document.getElementById('score');
-document.getElementById('cow').addEventListener('click',()=>{
- score++;
- scoreEl.textContent=score.toLocaleString();
-});
